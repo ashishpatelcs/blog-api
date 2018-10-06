01361a81d0807e7707be3c98cb85c5baca7e2c99
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-06 17:34:37 +0530

    add fs module to index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
+const fs = require('fs');
 const appconfig = require('./config/appConfig');
+const 
+
+const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello World!!!");
