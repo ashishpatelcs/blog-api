cee37e3c6b39e4e5a45dbbe6b083788c2f9e6dcd
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-06 16:22:11 +0530

    update appConfig.js and added appConfig.db with mongodb url

--- config/appConfig.js
+++ config/appConfig.js
@@ -4,6 +4,6 @@ appConfig.port = 3000
 appConfig.allowedCorsOrigin = "*"
 appConfig.env = "dev"
 appConfig.db = {
-
+    url: 'mongodb://test:test@127.0.0.1:27017/blogDB'
 }
 appConfig.apiVersion = '/api/v1'
