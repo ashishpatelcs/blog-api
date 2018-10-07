0b36b66b23783651e57a492197e1344c93b8c52e
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-08 04:08:20 +0530

    implement blog schema

--- models/Blog.js
+++ models/Blog.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let blogSchema = new Schema({
-    title: String,
-    
+    blogId: { type: String, unique: true },
+    title: { type: String, default: ''},
+    description: { type: String, default: '' },
+    bodyHtml: { type: String, default: '' },
+    views: { type: Number, default: 0 },
+    isPublished: { type: Boolean, default: false },
+    category: { type: String, default: '' },
+    author: { type: String, default: '' },
+    tags: [],
+    created: { type: Date, default: Date.now }
 })
