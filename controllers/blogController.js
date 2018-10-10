6f9b8f58ddd8c6b88389b23a7ca596b9582d8d30
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-11 01:21:28 +0530

    update blog controllers with standard response lib

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,19 +1,21 @@
 const Blog = require('../models/Blog')
 const shortid = require('shortid')
+const response = require('../libs/response')
 
 let getAllBlogs = (req, res) => {
     Blog.find({}, {_id: 0, __v: 0})
     .then(blogs => {
-        res.json({
-            status: 200,
-            message: 'success',
-            data: blogs
-        })
+        if (blogs === null || blogs === undefined || blogs === '') {
+            const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
+            res.json(apiResponse)
+        } else {
+            const apiResponse = response.generate(false, 'All blog details found', 200, blogs)
+            res.json(apiResponse)
+        }
     })
     .catch(error => {
-        res.json({
-            message: 'some error occured'
-        })
+        const apiResponse = response.generate(true, 'Error occured!', 500, null)
+        res.json(apiResponse)
     })
 }
 
@@ -21,66 +23,66 @@ let viewBlogById = (req, res) => {
     let blogId = req.params.blogId
     Blog.find({ blogId }, { _id: 0, __v: 0 })
     .then(blog => {
-        res.json({
-            status: 200,
-            message: 'success',
-            data: blog
-        })
+        if (blog === null || blog === undefined || blog === '') {
+            const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
+            res.json(apiResponse)
+        } else {
+            const apiResponse = response.generate(false, 'All blog details found', 200, blog)
+            res.json(apiResponse)
+        }
     })
     .catch(error => {
-        res.json({
-            message: 'some error occured' 
-        })
+        const apiResponse = response.generate(true, 'Error occured!', 500, null)
+        res.json(apiResponse)
     })
 }
 
 let viewBlogsByAuthor = (req, res) => {  
     let author = req.params.author
     Blog.find({ author }, { _id: 0, __v: 0 })
-    .then(blog => {
-        res.json({
-            status: 200,
-            message: 'success',
-            data: blog
-        })
+    .then(blogs => {
+        if (blogs === null || blogs === undefined || blogs === '') {
+            const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
+            res.json(apiResponse)
+        } else {
+            const apiResponse = response.generate(false, 'All blog details found', 200, blogs)
+            res.json(apiResponse)
+        }
     })
     .catch(error => {
-        res.json({
-            message: 'some error occured' 
-        })
+        const apiResponse = response.generate(true, 'Error occured!', 500, null)
+        res.json(apiResponse)
     })
 }
 
 let viewBlogsByCategory = (req, res) => {
     let category = req.params.category
     Blog.find({ category }, { _id: 0, __v: 0 })
-    .then(blog => {
-        res.json({
-            status: 200,
-            message: 'success',
-            data: blog
-        })
+    .then(blogs => {
+        if (blogs === null || blogs === undefined || blogs === '') {
+            const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
+            res.json(apiResponse)
+        } else {
+            const apiResponse = response.generate(false, 'All blog details found', 200, blogs)
+            res.json(apiResponse)
+        }
     })
     .catch(error => {
-        res.json({
-            message: 'some error occured' 
-        })
+        const apiResponse = response.generate(true, 'Error occured!', 500, null)
+        res.json(apiResponse)
     })
 }
 
 let deleteBlogById = (req, res) => { 
     let blogId = req.params.blogId
     Blog.findOneAndRemove({ blogId })
-    .then(post => {
-        res.json({
-            status: 200,
-            message: 'blog with id : ' + blogId + ' deleted successfully'
-        })
+    .then(blog => {
+            const apiResponse = response.generate(false, 'blog with id : ' + blogId + ' deleted successfully', 200, null)
+            res.json(apiResponse)
     })
     .catch(error => {
-        res.json({
-            message: 'some error occured' 
-        })
+        const apiResponse = response.generate(true, 'Error occured!', 500, null)
+        res.json(apiResponse)
     })
 }
 
@@ -90,16 +92,18 @@ let editBlogById = (req, res) => {
     let id = req.params.blogId
 
     Blog.findOneAndUpdate(id, blog, {new: true})
-    .then(post => {
-        res.json({
-            status: 200,
-            message: 'success'
-        })
+    .then(blog => {
+        if (blog === null || blog === undefined || blog === '') {
+            const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
+            res.json(apiResponse)
+        } else {
+            const apiResponse = response.generate(false, 'All blog details found', 200, blog)
+            res.json(apiResponse)
+        }
     })
     .catch(error => {
-        res.json({
-            message: 'some error occured' 
-        })
+        const apiResponse = response.generate(true, 'Error occured!', 500, null)
+        res.json(apiResponse)
     })
 }
 
@@ -111,17 +115,18 @@ let createBlog = (req, res) => {
     blog.tags = (blog.tags) ? blog.tags.split(',') : []
 
     Blog.create(blog)
-    .then(post => {
-        res.json({
-            status: 200,
-            message: 'success',
-            data: post
-        })
+    .then(blog => {
+        if (blog === null || blog === undefined || blog === '') {
+            const apiResponse = response.generate(true, 'No Blog Found', 404, null)
+            res.json(apiResponse)
+        } else {
+            const apiResponse = response.generate(false, 'Blog editted successfully', 200, blog)
+            res.json(apiResponse)
+        }
     })
     .catch(error => {
-        res.json({
-            message: 'some error occured' 
-        })
+        const apiResponse = response.generate(true, 'Error occured!', 500, null)
+        res.json(apiResponse)
     })
 }
 
@@ -133,23 +138,23 @@ let increaseBlogViews = (req, res) => {
         post.views += 1
         // console.log(post)
         Blog.findOneAndUpdate(blogId, post, {new: true})
-        .then(post => {
-            res.json({
-                status: 200,
-                message: 'success',
-                data: post
-            })
+        .then(blog => {
+            if (blog === null || blog === undefined || blog === '') {
+                const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
+                res.json(apiResponse)
+            } else {
+                const apiResponse = response.generate(false, 'blog views count increased!', 200, blog)
+                res.json(apiResponse)
+            }
         })
         .catch(error => {
-            res.json({
-                message: 'some error occured' 
-            })
+            const apiResponse = response.generate(true, 'Error occured!', 500, null)
+            res.json(apiResponse)
         })
     })
     .catch(error => {
-        res.json({
-            message: 'some error' 
-        })
+        const apiResponse = response.generate(true, 'Error occured!', 500, null)
+        res.json(apiResponse)
     })
 }
 
