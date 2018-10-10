e176d5ecb761fd4cbc5e532783ec7936e2cc9e14
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-10 06:45:29 +0530

    install cookie-parser and body-parser npm modules

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const appconfig = require('./config/appConfig')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Hello World!!!")
-})
+// middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 // bootstrap the routes
 let filesPath = './routes'
