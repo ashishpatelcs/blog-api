377fa362043b5f11cdffd737e9385f8c402f0cf4
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-11 02:43:03 +0530

    add capture error and info logs using logger lib

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -3,19 +3,23 @@ const shortid = require('shortid')
 const response = require('../libs/response')
 const time = require('../libs/time')
 const check = require('../libs/check')
+const logger = require('../libs/logger')
 
 let getAllBlogs = (req, res) => {
     Blog.find({}, {_id: 0, __v: 0})
     .then(blogs => {
         if (check.isEmpty(blogs)) {
+            logger.captureError('No Blogs Found', 'blogControler : getAllBlogs', 5)
             const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
             res.json(apiResponse)
         } else {
+            logger.captureInfo('All Blogs Found', 'blogControler : getAllBlogs', 0)
             const apiResponse = response.generate(false, 'All blog details found', 200, blogs)
             res.json(apiResponse)
         }
     })
     .catch(error => {
+        logger.captureError('Error Occured: ' + error, 'blogControler : getAllBlogs', 10)
         const apiResponse = response.generate(true, 'Error occured!', 500, null)
         res.json(apiResponse)
     })
@@ -26,14 +30,17 @@ let viewBlogById = (req, res) => {
     Blog.find({ blogId }, { _id: 0, __v: 0 })
     .then(blog => {
         if (check.isEmpty(blog)) {
+            logger.captureError('No Blog Found', 'blogControler : viewBlogById', 5)
             const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
             res.json(apiResponse)
         } else {
+            logger.captureInfo('Blog Found!', 'blogControler : viewBlogById', 0)
             const apiResponse = response.generate(false, 'blog with id ' + blogId + ' found!', 200, blog)
             res.json(apiResponse)
         }
     })
     .catch(error => {
+        logger.captureError('Error occured: ' + error, 'blogControler : viewBlogById', 10)
         const apiResponse = response.generate(true, 'Error occured!', 500, null)
         res.json(apiResponse)
     })
@@ -44,14 +51,17 @@ let viewBlogsByAuthor = (req, res) => {
     Blog.find({ author }, { _id: 0, __v: 0 })
     .then(blogs => {
         if (check.isEmpty(blogs)) {
+            logger.captureError('No Blogs Found', 'blogControler : viewBlogsByAuthor', 5)
             const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
             res.json(apiResponse)
         } else {
+            logger.captureInfo('All blog  with ' + author + ' as author found', 'blogControler : viewBlogsByAuthor', 0)
             const apiResponse = response.generate(false, 'All blog  with ' + author + ' as author found', 200, blogs)
             res.json(apiResponse)
         }
     })
     .catch(error => {
+        logger.captureError('Error occured: ' + error, 'blogControler : viewBlogsByAuthor', 10)
         const apiResponse = response.generate(true, 'Error occured!', 500, null)
         res.json(apiResponse)
     })
@@ -62,14 +72,17 @@ let viewBlogsByCategory = (req, res) => {
     Blog.find({ category }, { _id: 0, __v: 0 })
     .then(blogs => {
         if (check.isEmpty(blogs)) {
+            logger.captureError('No Blogs Found', 'blogControler : viewBlogsByCategory', 5)
             const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
             res.json(apiResponse)
         } else {
+            logger.captureInfo('All blog  with ' + category + ' category found', 'blogControler : viewBlogsByCategory', 0)
             const apiResponse = response.generate(false, 'All blog with ' + category + ' category found', 200, blogs)
             res.json(apiResponse)
         }
     })
     .catch(error => {
+        logger.captureError('Error occured: ' + error, 'blogControler : viewBlogsByCategory', 10)
         const apiResponse = response.generate(true, 'Error occured!', 500, null)
         res.json(apiResponse)
     })
@@ -79,10 +92,12 @@ let deleteBlogById = (req, res) => {
     let blogId = req.params.blogId
     Blog.findOneAndRemove({ blogId })
     .then(blog => {
+        logger.captureInfo('blog with id : ' + blogId + ' deleted successfully', 'blogControler : deleteBlogById', 0)
             const apiResponse = response.generate(false, 'blog with id : ' + blogId + ' deleted successfully', 200, null)
             res.json(apiResponse)
     })
     .catch(error => {
+        logger.captureError('Error occured: ' + error, 'blogControler : deleteBlogById', 10)
         const apiResponse = response.generate(true, 'Error occured!', 500, null)
         res.json(apiResponse)
     })
@@ -96,14 +111,17 @@ let editBlogById = (req, res) => {
     Blog.findOneAndUpdate(id, blog, {new: true})
     .then(blog => {
         if (check.isEmpty(blog)) {
+            logger.captureError('No Blogs Found', 'blogControler : editBlogById', 5)
             const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
             res.json(apiResponse)
         } else {
-            const apiResponse = response.generate(false, 'blog editted successfully', 200, blog)
+            logger.captureInfo('blog with id : ' + id + ' edited successfully', 'blogControler : editBlogById', 0)
+            const apiResponse = response.generate(false, 'blog edited successfully', 200, blog)
             res.json(apiResponse)
         }
     })
     .catch(error => {
+        logger.captureError('Error occured: ' + error, 'blogControler : editBlogById', 10)
         const apiResponse = response.generate(true, 'Error occured!', 500, null)
         res.json(apiResponse)
     })
@@ -121,14 +139,17 @@ let createBlog = (req, res) => {
     Blog.create(blog)
     .then(blog => {
         if (check.isEmpty(blog)) {
+            logger.captureError('No Blogs Found', 'blogControler : createBlog', 5)
             const apiResponse = response.generate(true, 'No Blog Found', 404, null)
             res.json(apiResponse)
         } else {
+            logger.captureInfo('blog created successfully!', 'blogControler : createBlog', 0)
             const apiResponse = response.generate(false, 'Blog created successfully', 200, blog)
             res.json(apiResponse)
         }
     })
     .catch(error => {
+        logger.captureError('Error occured: ' + error, 'blogControler : createBlog', 10)
         const apiResponse = response.generate(true, 'Error occured!', 500, null)
         res.json(apiResponse)
     })
@@ -144,9 +165,11 @@ let increaseBlogViews = (req, res) => {
         Blog.findOneAndUpdate(blogId, post, {new: true})
         .then(blog => {
             if (check.isEmpty(blog)) {
+                logger.captureError('No Blogs Found', 'blogControler : increaseBlogViews', 5)
                 const apiResponse = response.generate(true, 'No Blogs Found', 404, null)
                 res.json(apiResponse)
             } else {
+                logger.captureInfo('blog with id ' + blogId + ' views count increased!', 'blogControler : increaseBlogViews', 0)
                 const apiResponse = response.generate(false, 'blog views count increased!', 200, blog)
                 res.json(apiResponse)
             }
@@ -157,6 +180,7 @@ let increaseBlogViews = (req, res) => {
         })
     })
     .catch(error => {
+        logger.captureError('Error occured: ' + error, 'blogControler : increaseBlogViews', 10)
         const apiResponse = response.generate(true, 'Error occured!', 500, null)
         res.json(apiResponse)
     })
