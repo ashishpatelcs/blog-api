aec5797c9f681b9ac8349faca5446f845ead517a
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-10 07:04:22 +0530

    added route handlers to the blog controller

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,36 +1,34 @@
 function getAllBlogs(req, res) {
-
 }
 
 function viewBlogById(req, res) {
-
 }
 
-function viewBlogsByAuthor(req, res) {
-    
+function viewBlogsByAuthor(req, res) {  
 }
 
 function viewBlogsByCategory(req, res) {
-    
 }
 
-function deleteBlogById(req, res) {
-    
+function deleteBlogById(req, res) { 
 }
 
 function editBlogById(req, res) {
-    
 }
 
 function createBlog(req, res) {
-    
 }
 
 function increaseBlogViews(req, res) {
-    
 }
 
 module.exports = {
-    helloWorld: helloWorld,
-    exampleRoute: exampleRoute
+    getAllBlogs,
+    viewBlogById,
+    viewBlogsByAuthor,
+    viewBlogsByCategory,
+    deleteBlogById,
+    editBlogById,
+    createBlog,
+    increaseBlogViews
 }
