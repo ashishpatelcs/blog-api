372ab8fc1d02238e71a30fdc207ee9db603432ae
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-11 02:45:31 +0530

    update appErrorHandler.js to use logger lib

--- middlewares/appErrorHandler.js
+++ middlewares/appErrorHandler.js
@@ -1,12 +1,14 @@
 const response = require('../libs/response')
+const logger = require('../libs/logger')
 
 let errorHandler = (err, req, res, next) => {
-    console.log(err)
+    logger.captureError('Error occured: ' + err, 'appErrorHandler.js : errorHandler', 10)
     let apiResponse = response.generate(true, 'Some error occured', 500, null)
     res.json(apiResponse)
 }
 
 let notFoundHandler = (req, res, next) => {
+    logger.captureError('Error occured: Route not found in the applications', 'appErrorHandler.js : notFoundHandler', 10)
     let apiResponse = response.generate(true, 'Route not found in the application!', 404, null)
     res.status(404).json(apiResponse)
 }
