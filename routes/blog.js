2baf4c2d663a5cc13deb443d829c45d8e0927877
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-10 06:58:46 +0530

    add blog routes and controller handlers

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const blogcontroller = require('./../controllers/blogController')
+const appconfig = require('../config/appConfig')
 
 let setRouter = (app) => {
-    app.get('/', blogcontroller.homePage)
-
-    app.get('/hello-world', blogcontroller.helloWorld)
-    app.get('/example-route', blogcontroller.exampleRoute)
+    let BASEURL = appconfig.apiVersion + '/blogs'
+    app.get(BASEURL + '/all', blogcontroller.getAllBlogs)
+    app.get(BASEURL + '/view/:blodId', blogcontroller.viewBlogById)
+    app.get(BASEURL + '/view/author/:author', blogcontroller.viewBlogsByAuthor)
+    app.get(BASEURL + '/view/category/:category', blogcontroller.viewBlogsByCategory)
+    app.post(BASEURL + '/:blogId/delete', blogcontroller.deleteBlogById)
+    app.put(BASEURL + '/:blogId/edit', blogcontroller.editBlogById)
+    app.post(BASEURL + '/create', blogcontroller.createBlog)
+    app.post(BASEURL + '/:blogId/count/view', blogcontroller.increaseBlogViews)
 }
 
 module.exports = {
