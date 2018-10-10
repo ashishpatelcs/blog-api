01dd40a028d89c17fad85e4d314dc3064878aa96
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-11 01:37:08 +0530

    update appErrorHandler.js with standard response lib

--- middlewares/appErrorHandler.js
+++ middlewares/appErrorHandler.js
@@ -1,11 +1,15 @@
+const response = require('../libs/response')
 
 let errorHandler = (err, req, res, next) => {
     console.log(err)
-    res.send('some error occured at global level')
+    let apiResponse = response.generate(true, 'Some error occured', 500, null)
+    res.json(apiResponse)
 }
 
 let notFoundHandler = (req, res, next) => {
-    res.status(404).send('Route not found in the applicatoin!')
+    let apiResponse = response.generate(false, 'Route not found in the application!', 404, null)
+    res.status(404).json(apiResponse)
+    // res.status(404).send('Route not found in the applicatoin!')
 }
 
 module.exports = {
