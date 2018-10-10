7a901f61d332a7fd89092113b3e39c2d094d3b5d
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-11 02:23:57 +0530

    implement pino logger library

--- libs/logger.js
+++ libs/logger.js
@@ -2,5 +2,34 @@ const logger = require('pino')()
 const moment = require('moment')
 
 let captureError = (errorMessage, errorOrigin, errorLevel) => {
+    let timestamp = moment()
 
+    let errorResponse = {
+        timestamp,
+        errorMessage,
+        errorOrigin,
+        errorLevel
+    }
+
+    logger.error(errorResponse)
+    return errorResponse
+}
+
+let captureInfo = (message, origin, level) => {
+    let timestamp = moment()
+
+    let infoMessage = {
+        timestamp,
+        message,
+        origin,
+        level
+    }
+
+    logger.info(infoMessage)
+    return infoMessage
+}
+
+module.exports = {
+    captureError,
+    captureInfo
 }
