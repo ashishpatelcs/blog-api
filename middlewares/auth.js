47f13295852a2db13f5091ab2c95c1efcd6a2822
Ashish Patel <ashish.tripleclicks@gmail.com>
2018-10-11 05:40:59 +0530

    added authorization middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,10 @@
+const logger = require('../libs/logger')
+const response = require('../libs/response')
+const check = require('../libs/check')
+
+let isAuthenticated = (req, res, next) => {
+}
+
+module.exports = {
+    isAuthenticated
+}
